bc3081af7b4a63d003265736cc36bc6631a62212
Ilya <ilya0693@yandex.ru>
2021-03-25 17:17:09 +0300

    edit code "individual3_variant8"

--- individual3_variant8.py
+++ individual3_variant8.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import sys
-
-
 if __name__ == '__main__':
     for i in range(105, 701, 7):
-        d, u = divmod(i, 10)
-        h, d = divmod(d, 10)
+        d, u = i // 10, i % 10
+        h, d = d // 10, d % 10
         if h + d + u == 7:
             print(i)
